f107f7bcc8c50863942c1e63af300227f90f4e7a
Jitrocel <156829820+Jitrocel@users.noreply.github.com>
2024-01-16 20:16:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Add a marker at a specific location
 const marker_praha = L.marker([50.083, 14.4]).addTo(map); // San Francisco, CA coordinates
-const marker_smržovka = L.marker([14.7, 15.25]).addTo(map); // San Francisco, CA coordinates
+const marker_smrzovka = L.marker([14.7, 15.25]).addTo(map); // San Francisco, CA coordinates
 
 // Add a popup to the marker
 marker_praha.bindPopup("<b>Hello!</b><br>Praha").openPopup();
-marker_smržovka.bindPopup("<b>Hello!</b><br>Praha").openPopup();
+marker_smrzovka.bindPopup("<b>Hello!</b><br>Smržovka").openPopup();
