f2bd8eeb755972f5284ea217975b28a4fcce97c8
Jitrocel <156829820+Jitrocel@users.noreply.github.com>
2024-01-16 19:45:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initialize the map
-const map = L.map('map').setView([51, 15], 5); // Default view at coordinates [51, 15] and zoom level 5
+const map = L.map('map').setView([49.5, 15], 8); // Default view at coordinates [51, 15] and zoom level 5
 
 // Add a tile layer (you can choose different providers, like Mapbox)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
