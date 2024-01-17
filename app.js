f840ecbe47dea7302ce3892c739d6cad94fa2222
Jitrocel <156829820+Jitrocel@users.noreply.github.com>
2024-01-17 11:51:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const marker_praha = L.marker([50.083, 14.4]).addTo(map); // San Francisco, CA coordinates
 const marker_smrzovka = L.marker([50.7, 15.25]).addTo(map); // San Francisco, CA coordinates
 const marker_z_brod = L.marker([50.64, 15.25]).addTo(map); // San Francisco, CA coordinates
-const marker_z_brod = L.marker([49, 14.5]).addTo(map); // San Francisco, CA coordinates
+const marker_c_budejovice = L.marker([49, 14.5]).addTo(map); // San Francisco, CA coordinates
 
 
 // Add a popup to the marker
 marker_praha.bindPopup("<b>Hello!</b><br>Praha").openPopup();
 marker_smrzovka.bindPopup("<b>Hello!</b><br>Smržovka").openPopup();
 marker_z_brod.bindPopup("<b>Hello!</b><br>Železný Brod").openPopup();
-marker_z_brod.bindPopup("<b>Hello!</b><br>České Budějovice").openPopup();
+marker_c_budejovice.bindPopup("<b>Hello!</b><br>České Budějovice").openPopup();
