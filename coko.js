52919315ebfce3183a52197a05c4ef4888230f10
Tomin7T <157037488+Tomin7T@users.noreply.github.com>
2024-01-18 18:42:26 +0100

    Update coko.js

--- coko.js
+++ coko.js
@@ -47,3 +47,52 @@ svg.append("path")
     .attr("stroke", "red")
     .attr("stroke-width", 1.5)
     .attr("d", line);
+
+// Přidání osy x
+svg.append("g")
+    .attr("transform", "translate(0," + 300 + ")")
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+// Přidání osy y
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+// Přidání popisku osy x
+svg.append("text")             
+    .attr("transform", "translate(" + (500/2) + " ," + (300 + 40) + ")")
+    .style("text-anchor", "middle")
+    .text("Rok");
+
+// Přidání popisku osy y
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - 50)
+    .attr("x",0 - (300 / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Cena"); 
+
+// Přidání tooltipu
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+svg.selectAll("dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("r", 5)
+    .attr("cx", function(d) { return x(d.year); })
+    .attr("cy", function(d) { return y(d.price); })
+    .on("mouseover", function(d) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html("Rok: " + d.year + "<br/>"  + "Cena: " + d.price)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+    });
